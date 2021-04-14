f8e916f13d2bd9d81f46cc5f532f1f9008f09290
Jaap Heijligers <jaap@heijligers.me>
2021-04-14 14:40:25 +0200

    Update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,41 @@
 const name = "jatask-v1";
-const assets = ["/", "/style.css", "/bundle.js"];
+const assets = ["/", "/style.css", "/bundle.js", "/img/favicon-32x32.png"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(name)
-      .then((cache) => cache.addAll(assets))
-      .then(self.skipWaiting())
-  );
+    event.waitUntil(
+        caches
+            .open(name)
+            .then((cache) => cache.addAll(assets))
+            .then(self.skipWaiting())
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-  if (assets.indexOf(url.pathname) !== -1) {
-    event.respondWith(
-      caches
-        .match(event.request, {
-          ignoreSearch: true,
-        })
-        .then((res) => fetch(event.request) || res)
-    );
-  }
+    const url = new URL(event.request.url);
+    if (assets.indexOf(url.pathname) !== -1) {
+        event.respondWith(returnCache(event));
+    }
 });
+
+function returnCache(event) {
+    return caches.match(event.request)
+        .then(response => {
+            if (response) {
+                event.waitUntil(fetchAndCache(event.request));
+                return response;
+            }
+            return fetchAndCache(event.request);
+        });
+}
+
+
+function fetchAndCache(request) {
+    fetch(request).then(response => {
+        caches.open(name).then(cache => {
+            cache.put(request, response.clone());
+        });
+        return response;
+    }).catch(error => {
+        return Promise.reject(`Failed to fetch ${request.url}, ${error}`);
+    });
+}
