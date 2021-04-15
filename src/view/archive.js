d263c99de0f0309b0b28e5c88feb4cd1736b6bff
Jaap Heijligers <jaap@heijligers.me>
2021-04-15 18:05:18 +0200

    Archive dates

--- src/view/archive.js
+++ src/view/archive.js
@@ -1,27 +1,61 @@
 import { Component } from "./component";
 import { html } from "lit-html";
-import { tasks } from "../model/task";
+import { Task, tasks } from "../model/task";
 
 export class Archive extends Component {
   constructor() {
     super("Archive");
   }
 
+  /**
+   * @returns {Task[]}
+   */
   archivedTasks() {
-    return tasks.filter((t) => t.isCompleted());
+    const archiveTasks = tasks.filter((t) => t.isCompleted());
+    return archiveTasks;
+    const length = archiveTasks.length;
+    return archiveTasks.slice(Math.max(0, length - 10));
+  }
+
+  archiveData() {
+    const aTasks = this.archivedTasks();
+    const data = [];
+    let date = null;
+    for (const t of aTasks) {
+      if (date == null || t.createdAt.getDate() !== date.getDate()) {
+        date = t.createdAt;
+        data.push(date);
+      }
+      data.push(t);
+    }
+    return data;
   }
 
   render() {
     return html`
       <ul class="archived-tasks">
-        ${this.archivedTasks().map(
-          (t) => html`
-            <li>
-              <span>${t.name}</span>
-            </li>
-          `
-        )}
+        ${this.archiveData().map((item) => this.renderItem(item))}
       </ul>
     `;
+  }
+
+  renderItem(item) {
+    if (item instanceof Task) {
+      return html`
+        <li>
+          <div class="task-card">
+            <div class="body">
+              <div class="title">${item.name}</div>
+            </div>
+          </div>
+        </li>
+      `;
+    } else if (item instanceof Date) {
+      return html`<li class="date">${this.formatDate(item)}</li>`;
+    }
+  }
+
+  formatDate(date) {
+    return date.toLocaleDateString();
   }
 }
